3ae5bec996b4de59bd26b75e6b65b13c65209a44
Jorge <Jorge.A.Valencia.-ND@disney.com>
2022-06-21 14:44:16 -0500

    Add arcade exercise number 23
    Part of Island of knowledge section

--- src/com/codesignal/arcade/intro/islandofknowledge/BoxBlur.java
+++ src/com/codesignal/arcade/intro/islandofknowledge/BoxBlur.java
@@ -1,14 +1,13 @@
 package com.codesignal.arcade.intro.islandofknowledge;
 
-import java.util.Arrays;
-
 public class BoxBlur {
     public static void main(String[] args) {
         int[][] image = {
-            { 7, 4, 0, 1 },
-            { 5, 6, 2, 2 },
-            { 6, 10, 7, 8},
-            { 1, 4, 2, 0 }};
+                { 7, 4, 0, 1 },
+                { 5, 6, 2, 2 },
+                { 6, 10, 7, 8 },
+                { 1, 4, 2, 0 }
+        };
 
         int[][] blurredImage = boxBlur(image);
 
@@ -20,14 +19,18 @@ public class BoxBlur {
         final int rows = image.length - 2;
         final int cols = image[0].length - 2;
         int[][] solution = new int[rows][cols];
-        for (int i = 0; i < cols; i++) {
-            for (int j = i; j < rows; j++) {
-                int sum = 0;
-                for (int k = i; k < j + 3; k++) {
-                    int[] subRow = Arrays.copyOfRange(image[k], k, k + 3);
-                    sum += Arrays.stream(subRow).sum();
-                }
-                solution[j][i] = sum / 9;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                int sum = image[i - 1][j - 1] +
+                        image[i - 1][j] +
+                        image[i - 1][j + 1] +
+                        image[i][j - 1] +
+                        image[i][j] +
+                        image[i][j + 1] +
+                        image[i + 1][j - 1] +
+                        image[i + 1][j] +
+                        image[i + 1][j + 1];
+                solution[i - 1][j - 1] = sum / 9;
             }
         }
 
